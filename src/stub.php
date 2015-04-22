78f95b450c3ed8d49c7e6d7c56b29cf4b327f6a4
jaswsinc <jas@wsharks.com>
2015-04-22 12:58:19 -0800

    Adding Phing build file.

---
+++ src/stub.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * HTML Compressor Stub.
+ *
+ * @since 150421 Improving PHAR support.
+ *
+ * @author JasWSInc <https://github.com/jaswsinc>
+ * @copyright WebSharks, Inc. <http://www.websharks-inc.com>
+ * @license GNU General Public License, version 2
+ */
+namespace WebSharks\HtmlCompressor;
+
+require_once dirname(__FILE__).'/includes/classes/Core.php';
